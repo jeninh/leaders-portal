9751df8c6c0a93d14930940c52ee0227cd0a2323
Charmunks <isaac@jedfonner.com>
2025-12-09 09:52:48 -0500

    throw 404 on club not found

--- src/routes/join/[code]/+page.server.js
+++ src/routes/join/[code]/+page.server.js
@@ -1,11 +1,17 @@
+import { error } from '@sveltejs/kit';
 import { getClubByCode } from '$lib/server/clubapi.js';
 
 export async function load({ params }) {
 	const { code } = params;
 	const club = await getClubByCode(code);
+	const clubName = club?.fields?.club_name || club?.club_name;
+	
+	if (!clubName) {
+		throw error(404, 'Club not found');
+	}
 	
 	return {
 		code,
-		clubName: club?.fields?.club_name || club?.club_name || null
+		clubName
 	};
 }
